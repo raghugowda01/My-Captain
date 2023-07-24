de423928037f23ca26d0cac78c53ed89ba3e8ab7
raghugowda01 <140423771+raghugowda01@users.noreply.github.com>
2023-07-24 21:23:32 +0530

    Create Fibonacci with recursion.java

---
+++ Fibonacci with recursion.java
@@ -0,0 +1,18 @@
+ //FibonacciWithRecursion
+public class FibonacciWithRecursion {
+    public static int fibonacci(int n) {
+        if (n <= 1) {
+            return n;
+        }
+        return fibonacci(n - 1) + fibonacci(n - 2);
+    }
+
+    public static void main(String[] args) {
+        int n = 10; // Number of terms in the Fibonacci series
+
+        System.out.println("Fibonacci Series:");
+        for (int i = 0; i < n; i++) {
+            System.out.print(fibonacci(i) + " ");
+        }
+    }
+}
